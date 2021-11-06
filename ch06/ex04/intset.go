3a510413ade3c11cf98952561981d4c2231d99f0
Sanosuke Kato <8940110+sanopy@users.noreply.github.com>
2021-11-06 18:04:07 +0900

    :sparkles: done section 6.5

---
+++ ch06/ex04/intset.go
@@ -0,0 +1,154 @@
+// Package intset provides a set of integers based on a bit vector.
+package intset
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// AddAll adds the non-negative values to the set.
+func (s *IntSet) AddAll(vals ...int) {
+	for _, v := range vals {
+		s.Add(v)
+	}
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// IntersectWith sets s to the intersect of s and t.
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		} else {
+			break
+		}
+	}
+}
+
+// DifferenceWith sets s to the difference of s and t.
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= s.words[i] & tword
+		} else {
+			break
+		}
+	}
+}
+
+// SymmetricWith sets s to the Symmetric of s and t.
+func (s *IntSet) SymmetricWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Len returns the number of elements in the set.
+func (s *IntSet) Len() int {
+	len := 0
+	for _, w := range s.words {
+		len += popcount(w)
+	}
+	return len
+}
+
+func popcount(x uint64) int {
+	res := 0
+	for x > 0 {
+		x &= x - 1
+		res++
+	}
+	return res
+}
+
+// Remove removes the value x from the set.
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word < len(s.words) {
+		s.words[word] &= s.words[word] ^ 1<<bit
+	}
+}
+
+// Clear removes all values from the set.
+func (s *IntSet) Clear() {
+	s.words = []uint64{}
+}
+
+// Copy returns the copy of the set.
+func (s *IntSet) Copy() *IntSet {
+	r := new(IntSet)
+	r.words = make([]uint64, s.Len())
+	copy(r.words, s.words)
+	return r
+}
+
+// Elems returns the slice including elements of the set.
+func (s *IntSet) Elems() []uint64 {
+	elems := []uint64{}
+	for i, w := range s.words {
+		for w > 0 {
+			lsb := -w & w
+			bit := int(math.Log2(float64(lsb)))
+			elems = append(elems, uint64(i*64+bit))
+			w ^= lsb
+		}
+	}
+	return elems
+}
---
+++ ch06/ex04/intset_test.go
@@ -0,0 +1,180 @@
+package intset
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHas(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(64)
+	t.Run("exist target element", func(t *testing.T) {
+		want := true
+		if got := x.Has(64); got != want {
+			t.Errorf("actual: %v, want: %v", got, want)
+		}
+	})
+	t.Run("not exist target element", func(t *testing.T) {
+		want := false
+		if got := x.Has(63); got != want {
+			t.Errorf("actual: %v, want: %v", got, want)
+		}
+	})
+}
+
+func TestAdd(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(63)
+	x.Add(64)
+	x.Add(127)
+	want := "{0 63 64 127}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestAddAll(t *testing.T) {
+	var x IntSet
+	x.AddAll(0, 63, 64, 127)
+	want := "{0 63 64 127}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestUnionWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(0)
+	x.Add(64)
+	x.Add(127)
+	y.Add(0)
+	y.Add(63)
+	y.Add(128)
+	x.UnionWith(&y)
+	want := "{0 63 64 127 128}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestIntersectWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(0)
+	x.Add(64)
+	x.Add(127)
+	y.Add(0)
+	y.Add(63)
+	y.Add(127)
+	y.Add(128)
+	x.IntersectWith(&y)
+	want := "{0 127}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestDifferenceWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(0)
+	x.Add(1)
+	x.Add(64)
+	x.Add(127)
+	y.Add(0)
+	y.Add(63)
+	y.Add(127)
+	y.Add(128)
+	x.DifferenceWith(&y)
+	want := "{1 64}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestSymmetricWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(0)
+	x.Add(1)
+	x.Add(64)
+	x.Add(127)
+	y.Add(0)
+	y.Add(63)
+	y.Add(127)
+	y.Add(128)
+	x.SymmetricWith(&y)
+	want := "{1 63 64 128}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestLen(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(63)
+	x.Add(64)
+	x.Add(127)
+	want := 4
+	if got := x.Len(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(63)
+	x.Add(64)
+	x.Add(127)
+	t.Run("not exist target element", func(t *testing.T) {
+		want := "{0 63 64 127}"
+		x.Remove(1)
+		x.Remove(128)
+		if got := x.String(); got != want {
+			t.Errorf("actual: %v, want: %v", got, want)
+		}
+	})
+	t.Run("exist target element", func(t *testing.T) {
+		want := "{0 63 127}"
+		x.Remove(64)
+		if got := x.String(); got != want {
+			t.Errorf("actual: %v, want: %v", got, want)
+		}
+	})
+}
+
+func TestClear(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(63)
+	x.Add(64)
+	x.Add(127)
+	x.Clear()
+	want := "{}"
+	if got := x.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestCopy(t *testing.T) {
+	var x IntSet
+	x.Add(0)
+	x.Add(63)
+	x.Add(64)
+	x.Add(127)
+	y := x.Copy()
+	want := "{0 63 64 127}"
+	if got := y.String(); got != want {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
+
+func TestElms(t *testing.T) {
+	var x IntSet
+	x.AddAll(0, 63, 64, 127)
+	want := []uint64{0, 63, 64, 127}
+	if got := x.Elems(); !reflect.DeepEqual(got, want) {
+		t.Errorf("actual: %v, want: %v", got, want)
+	}
+}
