0a45c1cf4b14e3ae717953950c01639fd75e3a78
Sanosuke Kato <8940110+sanopy@users.noreply.github.com>
2021-11-19 15:59:12 +0900

    :hammer: update run.sh for testing 32bit and 64bit architecture

--- ch06/ex05/run.sh
+++ ch06/ex05/run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 go test -v
+
+# for linux; `GOARCH=386` is not supported for darwin
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # 64bit
+  GOARCH=amd64 go test -v
+
+  # 32bit
+  GOARCH=386 go test -v
+fi
