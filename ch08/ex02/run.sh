566d443bbe69621ebba9f0fd3a0ea7d30a18a457
Sanosuke Kato <8940110+sanopy@users.noreply.github.com>
2021-12-30 16:33:54 +0900

    :pencil2: fix port number

--- ch08/ex02/run.sh
+++ ch08/ex02/run.sh
@@ -2,6 +2,6 @@
 
 echo "ftp server ready..."
 echo "for example, you can connect to a chat server with the following command."
-echo "  $ ftp localhost 8000"
+echo "  $ ftp localhost 2100"
 
 go run *.go
